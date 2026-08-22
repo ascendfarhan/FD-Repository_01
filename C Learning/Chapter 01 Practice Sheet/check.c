c06f7b7d7f810e6c0857fc466d2e076329a2f503
Farhan Danish <www.farhandanish@gmail.com>
2026-08-23 00:35:45 +0530

    Update check.c

--- C Learning/Chapter 01 Practice Sheet/check.c
+++ C Learning/Chapter 01 Practice Sheet/check.c
@@ -2,6 +2,7 @@
 
 int main()
 {
-    printf("just cheking features of vs code and github integration");
+    printf("just cheking features of vs code and github integration\n");
+    printf("Cheking In Phone");
     return 0;
 };
